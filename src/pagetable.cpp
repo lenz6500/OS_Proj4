84badc8574fc47f0d71d2d6d2f259a2313ef71c6
lenz6500 <lenz6500@stthomas.edu>
2020-04-20 17:37:38 -0500

    Page Tables

--- src/pagetable.cpp
+++ src/pagetable.cpp
@@ -1,4 +1,5 @@
 #include "pagetable.h"
+#include <math.h>
 
 PageTable::PageTable(int page_size)
 {
@@ -23,9 +24,29 @@ void PageTable::addEntry(uint32_t pid, int page_number)
 int PageTable::getPhysicalAddress(uint32_t pid, int virtual_address)
 {
     // Convert virtual address to page_number and page_offset
+
+	int bitOffset = 0;
+	int page_size_holder = _page_size;
+
+	//First offset;
+	bitOffset = 1;
+	page_size_holder>>1;
+
+	//Remaining offsets
+	while(page_size_holder > 1){
+		bitOffset + 1;
+		page_size_holder>>1;
+
+	}
+
+	printf("%d is bit offset;\n", bitOffset);
+
     // TODO: implement this!
-    int page_number = 0;
-    int page_offset = 0;
+    int page_number = virtual_address >> bitOffset;
+    int page_offset = virtual_address && (pow(2, bitOffset)-1);
+
+	printf("%d is page number;\n", page_number);
+	printf("%d is page offset;\n", page_offset);
 
     // Combination of pid and page number act as the key to look up frame number
     std::string entry = std::to_string(pid) + "|" + std::to_string(page_number);
