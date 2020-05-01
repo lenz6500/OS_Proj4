94209a29ac985af5c8604030b8e2a73cdb224cc0
Yuzu17 <suzu4287@stthomas.edu>
2020-04-30 21:40:03 -0500

    Update mmu.cpp

--- src/mmu.cpp
+++ src/mmu.cpp
@@ -181,8 +181,7 @@ int Mmu::set(uint8_t *memory, uint32_t pid, std::string& var_name, uint32_t offs
 	return 0;
 }
 
-int Mmu::free(uint32_t pid, std::string& var_name)
-
+int Mmu::free(uint32_t pid, std::string& var_name, PageTable *pageTable)
 {
 	int index = findProcess(pid);
 	if( index == -1 )
